854cee19ea6f1c041cde17b9eb76883dc784b09b
A_Peng215 <976310604@qq.com>
2023-10-04 12:57:36 +0800

    Remove useless keybindings.

--- src/main/java/net/apeng/filtpick/event/ClientEvents.java
+++ src/main/java/net/apeng/filtpick/event/ClientEvents.java
@@ -28,22 +28,6 @@ public class ClientEvents {
     @Mod.EventBusSubscriber(modid = FiltPick.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE)
     public static class ClientForgeEvents {
 
-        @SubscribeEvent
-        // Event is on the Forge event bus only on the physical client
-        public static void onKeyPressed(TickEvent.ClientTickEvent event) {
-            if (event.phase == TickEvent.Phase.END) { // Only call code once as the tick event is called twice every tick
-                while (KeyBinding.OPEN_FILTLIST_MAP.consumeClick()) {
-                    // Execute logic to perform on click here
-                    NetWorkHandler.sendToServer(new OpenFiltScreenC2SPacket());
-                }
-                while (KeyBinding.SET_ITEM_MAP.consumeClick()) {
-                    // Execute logic to perform on click here
-                    FiltPick.CLIENT_FILT_LIST.setStackInSlot(0, Minecraft.getInstance().player.getMainHandItem());
-                    NetWorkHandler.sendToServer(new SynFiltModesC2SPacket(FiltPick.CLIENT_FILT_LIST));
-                }
-
-            }
-        }
 
         @SubscribeEvent
         public static void addEntryButton(ScreenEvent.Init.Post event) {
@@ -67,17 +51,7 @@ public class ClientEvents {
 
     @Mod.EventBusSubscriber(modid = FiltPick.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onKeyRegister(RegisterKeyMappingsEvent event) {
-            event.register(KeyBinding.SET_ITEM_MAP);
-            event.register(KeyBinding.OPEN_FILTLIST_MAP);
-        }
 
-
-        @SubscribeEvent
-        public static void gatherData(GatherDataEvent event) {
-
-        }
 
         @SubscribeEvent
         public static void registerScreens(FMLClientSetupEvent event) {
