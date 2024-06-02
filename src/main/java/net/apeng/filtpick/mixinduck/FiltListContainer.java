d308d4227f3ccd8dcb4a7fc0948fe56294327fb7
A_Peng215 <976310604@qq.com>
2024-06-02 10:20:53 +0800

    Add annotation

--- src/main/java/net/apeng/filtpick/mixinduck/FiltListContainer.java
+++ src/main/java/net/apeng/filtpick/mixinduck/FiltListContainer.java
@@ -3,12 +3,18 @@ package net.apeng.filtpick.mixinduck;
 import net.apeng.filtpick.property.FiltListPropertyDelegate;
 import net.minecraft.world.SimpleContainer;
 
+/**
+ * Indicate a class can provide filtlist objects including filtlist and list properties.
+ */
 public interface FiltListContainer {
 
     SimpleContainer getFiltList();
 
     FiltListPropertyDelegate getFiltListPropertyDelegate();
 
+    /**
+     * Reset filtlist and its properties
+     */
     void resetFiltListWithProperties();
 
 }
